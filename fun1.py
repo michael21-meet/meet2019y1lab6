113d1599a97ba273d1a77bcf36bff00fba276736
michael21-meet <michael21@meet.mit.edu>
2019-07-28 09:26:43 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,15 @@
+def add_numbers(start,end):
+    total = 0
+    for number in range(1, 10 + 1):
+        print(number)
+        total = total + number
+    return(total)
+test1 = add_numbers(1,2)
+print(test1)
+test2 = add_numbers(1, 100)
+print(test2)
+test3 = add_numbers(1000, 5000)
+print(test3)
+1
+answer = add_numbers()
+print(answer)
