a1b56bdaa368221c9ec00475b9b780c73e0241c1
michael21-meet <michael21@meet.mit.edu>
2019-07-29 05:28:35 -0400

    Stopping here

--- lab_6.py
+++ lab_6.py
@@ -1,14 +1,13 @@
-def add_numbers(start,end):
+def add_numbers(start, end):
     total = 0
-    for number in range(start, end + 1):
+    for number in range(333, 777):
         print(number)
         total = total + number
     return(total)
+
 test1 = add_numbers(1,2)
 print(test1)
 test2 = add_numbers(1, 100)
 print(test2)
 test3 = add_numbers(1000, 5000)
 print(test3)
-answer = add_numbers(1, 5000)
-print(answer)
